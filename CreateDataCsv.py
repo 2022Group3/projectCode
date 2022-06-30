3cfc794ea89b0052322d072723a9d70ea1a6d876
2022Group3 <group3databootcamp@gmail.com>
2022-07-01 00:22:09 +0300

    change batch_df function to short one

--- CreateDataCsv.py
+++ CreateDataCsv.py
@@ -54,33 +54,53 @@ def cfar100ToDf(picklePath, chosen_label=2):
 
 # create dataFrame from batch
 def batch_df(dict,label_names,batch,batch_num):
-    df=pd.DataFrame()
-    #id
-    indexes=np.arange(batch_num*10000-9999,batch_num*10000+1)
-    df['id']= indexes
-    #image name
-    df['image_name']=dict[b'filenames']
-    #bach label:
-    batch_array=np.full((len(dict[b'labels']),1),batch)
-    df['batch_label']=batch_array
-    # label num
-    df['label_number']=dict[ b'labels']
-    #label names
-    label_names_array=[]
+    df = pd.DataFrame({'id':np.arange(batch_num*10000-9999,batch_num*10000+1),'image_name':dict[b'filenames'],
+                       'batch_label':batch,'label_number':dict[ b'labels'],'dataset':'cifar10'})
+    # label names
+    label_names_array = []
     for i in df['label_number']:
         label_names_array.append(label_names[i])
-    df['label_name']=label_names_array
-    #dataset
-    dataset_array=np.full((len(dict[b'labels']),1),'cifar10')
-    df['dataset']=dataset_array
-    #tain/validation/test
+    df['label_name'] = label_names_array
+    # tain/validation/test
     if batch=='test_batch':
         type='test'
     else:
         type='train'
     data_type=np.full((len(dict[b'labels']),1),type)
     df['tain/validation/test']=data_type
+    # reorder columns
+    cols=['id','image_name','batch_label','label_number','label_name','dataset','tain/validation/test']
+    chosen_label_df = df[cols]
     return df
+
+# def batch_df(dict,label_names,batch,batch_num):
+#     df=pd.DataFrame()
+#     #id
+#     indexes=np.arange(batch_num*10000-9999,batch_num*10000+1)
+#     df['id']= indexes
+#     #image name
+#     df['image_name']=dict[b'filenames']
+#     #bach label:
+#     batch_array=np.full((len(dict[b'labels']),1),batch)
+#     df['batch_label']=batch_array
+#     # label num
+#     df['label_number']=dict[ b'labels']
+#     #label names
+#     label_names_array=[]
+#     for i in df['label_number']:
+#         label_names_array.append(label_names[i])
+#     df['label_name']=label_names_array
+#     #dataset
+#     dataset_array=np.full((len(dict[b'labels']),1),'cifar10')
+#     df['dataset']=dataset_array
+#     #tain/validation/test
+#     if batch=='test_batch':
+#         type='test'
+#     else:
+#         type='train'
+#     data_type=np.full((len(dict[b'labels']),1),type)
+#     df['tain/validation/test']=data_type
+#     return df
 
 
 # create dataFrame from cifar10
@@ -100,10 +120,10 @@ def cifar10Df(batch_file,label_names):
 
 
 if __name__ == '__main__':
-     batch_file=r"C:\פרוייקט בוטקמפ\dataset\cifar-10-batches-py"
-     meta_file = r"C:\פרוייקט בוטקמפ\dataset\cifar-10-batches-py\batches.meta"
+     batch_file=r"C:\D\bootcamp\project\dataset\cifar-10-batches-py"
+     meta_file = r"C:\D\bootcamp\project\dataset\cifar-10-batches-py\batches.meta"
      meta_data = unpickle(meta_file)
      label_names = meta_data[b'label_names']
      cifar10Df(batch_file,label_names)
-     cfar100ToDf("C:\פרוייקט בוטקמפ\dataset\cifar-100-python")
+     cfar100ToDf(r"C:\D\bootcamp\project\dataset\cifar-100-python")
      #hello
