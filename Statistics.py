93ab4bf64aef3a7c1a39ba176bbd254be6740b6d
2022Group3 <group3databootcamp@gmail.com>
2022-07-03 16:37:56 +0300

    first

--- Statistics.py
+++ Statistics.py
@@ -1,15 +1,30 @@
 #This file will show different statistics on our csv
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 class Statistics:
     def __init__(self,csv):
         self.df = pd.read_csv(csv)
-        print (self.df)
+        # print (self.df)
 
     def dataSize(self):
         print(len(self.df))
 
-if __name__ == '__main__':
-    st=Statistics(r"C:\פרוייקט בוטקמפ\projectCode\data.csv")
+    def distPerClass(self):
+        sns.countplot(x='label_name', data=self.df)
+        # sns.distplot(trainDF['label_name'])
+        plt.show()
 
-    st.dataSize()
+    def typeDistPerClass(self, type):
+        typeDF=self.df[self.df['train/validation/test']==type]
+        sns.countplot(x='label_name', data=typeDF)
+        plt.show()
+
+#
+# if __name__ == '__main__':
+#     st=Statistics(r"C:\פרוייקט בוטקמפ\projectCode\data.csv")
+#     st.dataSize()
+#     # st.distPerClass()
+#     st.typeDistPerClass('test')
