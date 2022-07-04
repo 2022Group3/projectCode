126e34565598851d9a27ce7500a3ee4d4cf71fa1
lea-cohen <l0548452382@gmail.com>
2022-07-04 14:25:38 +0300

    display function

--- display.py
+++ display.py
@@ -6,20 +6,23 @@ from matplotlib import pyplot as plt
 def display_image(imageArray):
 
     array=imageArray
-    array = np.reshape(array, (32,32,3))
-    im = Image.fromarray(array)
+    rgbArray = np.zeros((32, 32, 3), 'uint8')
+    rgbArray[..., 0] = array[:1024].reshape(32, 32)
+    rgbArray[..., 1] = array[1024:2048].reshape(32, 32)
+    rgbArray[..., 2] = array[2048:3072].reshape(32, 32)
+    im = Image.fromarray(rgbArray)
     im.save("filename.jpeg")
     im.show()
 
 #def unpickle(file):
- #   import pickle
-   # with open(file, 'rb') as fo:
-   #     dict = pickle.load(fo, encoding='bytes')
-   # return dict
+    #import pickle
+    #with open(file, 'rb') as fo:
+       #dict = pickle.load(fo, encoding='bytes')
+   #return dict
 if __name__ == '__main__':
    #batch_file=r"C:\הנדסאים שנה ב\בוטקאמפ\פרוייקט\cifar-10-python (1)\data_batch_3"
-  # dict=unpickle(batch_file)
-   display_image(dict[b'data'][1000])
+   #dict=unpickle(batch_file)
+   display_image(dict[b'data'][50])
 
    #im = Image.open(r"C:\Users\1\Downloads\86289.jpg")
    #im.show()
