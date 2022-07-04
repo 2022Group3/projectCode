73c5c3e3281a0f49b65816b5704700efa24bb072
Nechama <neproject2@gmail.com>
2022-07-04 17:10:04 +0300

    add 5 super classes from cfar100

--- CreateDataCsv.py
+++ CreateDataCsv.py
@@ -20,7 +20,7 @@ def data_to_csv(data,cols=False):
 
 
 # create dataFrame from cfar100
-def cfar100ToDf(picklePath,chosen_label=2):
+def cfar100ToDf(picklePath,chosen_label=[1,2,3,4,5]):
     train = unpickle(picklePath + "\\train")
     test = unpickle(picklePath + "\\test")
     names=unpickle(picklePath+"\\meta")[b'coarse_label_names']
@@ -42,8 +42,8 @@ def cfar100ToDf(picklePath,chosen_label=2):
 
 
     # filter class, append train+test
-    chosen_label_df = df_train[df_train['label_number'] == chosen_label].append(
-        df_test[df_test['label_number'] == chosen_label])
+    chosen_label_df = df_train[df_train['label_number'].isin(chosen_label)].append(
+        df_test[df_test['label_number'].isin(chosen_label)])
     chosen_label_df['label_name'] = chosen_label_df['label_number'].apply(create_labelname)
 
     print(chosen_label_df)
@@ -88,14 +88,15 @@ def cifar10Df(batch_file,label_names,cols):
     data_to_csv(df)
 # Press the green button in the gutter to run the script.
 
-#
-# if __name__ == '__main__':
-#      batch_file=r"C:\D\bootcamp\project\dataset\cifar-10-batches-py"
-#      meta_file = r"C:\D\bootcamp\project\dataset\cifar-10-batches-py\batches.meta"
-#      meta_data = unpickle(meta_file)
-#      label_names = meta_data[b'label_names']
-#      cols = ['image_name','batch_label','label_number','label_name','dataset','train/validation/test']
-#      cifar10Df(batch_file,label_names,cols)
-#      cfar100_file=r"C:\D\bootcamp\project\dataset\cifar-100-python"
-#      cfar100ToDf(cfar100_file)
+
+
+if __name__ == '__main__':
+     batch_file=r"C:\פרוייקט בוטקמפ\dataset\cifar-10-batches-py"
+     meta_file = r"C:\פרוייקט בוטקמפ\dataset\cifar-10-batches-py\batches.meta"
+     meta_data = unpickle(meta_file)
+     label_names = meta_data[b'label_names']
+     cols = ['image_name','batch_label','label_number','label_name','dataset','train/validation/test']
+     cifar10Df(batch_file,label_names,cols)
+     cfar100_file=r"C:\פרוייקט בוטקמפ\dataset\cifar-100-python"
+     cfar100ToDf(cfar100_file)
 
--- display.py
+++ display.py
@@ -3,9 +3,8 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 
-def display_image(imageArray):
+def display_image(array):
 
-    array=imageArray
     rgbArray = np.zeros((32, 32, 3), 'uint8')
     rgbArray[..., 0] = array[:1024].reshape(32, 32)
     rgbArray[..., 1] = array[1024:2048].reshape(32, 32)
@@ -14,15 +13,19 @@ def display_image(imageArray):
     im.save("filename.jpeg")
     im.show()
 
-#def unpickle(file):
-    #import pickle
-    #with open(file, 'rb') as fo:
-       #dict = pickle.load(fo, encoding='bytes')
-   #return dict
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+       dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+
 if __name__ == '__main__':
-   #batch_file=r"C:\הנדסאים שנה ב\בוטקאמפ\פרוייקט\cifar-10-python (1)\data_batch_3"
-   #dict=unpickle(batch_file)
-   display_image(dict[b'data'][50])
+   batch_file=r"C:\פרוייקט בוטקמפ\dataset\cifar-10-batches-py\data_batch_1"
+   dict=unpickle(batch_file)
+   display_image(dict[b'data'][4])
 
    #im = Image.open(r"C:\Users\1\Downloads\86289.jpg")
    #im.show()
