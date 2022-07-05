7ebcb48d2e300f0ca37bd56257cdd933f2d8b59f
2022Group3 <group3databootcamp@gmail.com>
2022-07-05 17:02:28 +0300

    extract images

--- extractImagesFromPickle.py
+++ extractImagesFromPickle.py
@@ -1,23 +1,23 @@
 from __future__ import print_function
 import numpy as np
-import cPickle
+import pickle
 import cv2
 import os
 
 def load_cifar_pickle(path, file):
     f = open(os.path.join(path, file), 'rb')
-    dict = cPickle.load(f)
-    images = dict['data']
+    dict = pickle.load(f,encoding='bytes')
+    images = dict[b'data']
     images = np.reshape(images, (10000, 3, 32, 32))
-    labels = np.array(dict['labels'])
+    labels = np.array(dict[b'labels'])
     print("Loaded {} labelled images.".format(images.shape[0]))
     return images, labels
 
 
 def load_cifar_categories(path, file):
     f = open(os.path.join(path, file), 'rb')
-    dict = cPickle.load(f)
-    return dict['label_names']
+    dict = pickle.load(f,encoding='bytes')
+    return dict[b'label_names']
 
 def save_cifar_image(array, path):
     # array is 3x32x32. cv2 needs 32x32x3
@@ -29,16 +29,19 @@ def save_cifar_image(array, path):
 
 
 if __name__ == '__main__':
-    base_dir= './data'
+    base_dir= r"C:\D\bootcamp\project\dataset"
     picke_name = 'data_batch_1'
     n_imgs = 10
 
     images, labels = load_cifar_pickle(os.path.join(base_dir, 'cifar-10-batches-py'), picke_name)
     categories = load_cifar_categories(os.path.join(base_dir, 'cifar-10-batches-py'), "batches.meta")
     print(categories)
+
     for i in range(0,n_imgs):
         cat = categories[labels[i]]
-        out_dir = os.path.join(base_dir, 'cifar10', cat)
+        cat=cat.decode('utf-8')
+        print(cat)
+        out_dir = os.path.join(base_dir, 'datasetImages', cat)
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
