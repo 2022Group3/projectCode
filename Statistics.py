fec7fdd574aa01d1467b929a8b46d79f3e8a8a83
2022Group3 <group3databootcamp@gmail.com>
2022-07-05 15:52:22 +0300

    not class

--- Statistics.py
+++ Statistics.py
@@ -4,90 +4,84 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+file=r"C:\Users\אפרת\Downloads\df.csv"
+df =pd.read_csv(file)
 
-class Statistics:
-    def __init__(self,csv):
-        self.df = pd.read_csv(csv)
-        # print (self.df)
+def dataSize():
+    print(len(df))
 
-    def dataSize(self):
-        print(len(self.df))
+def distPerClass():
+    sns.countplot(x='label_name', data=df)
+    # sns.distplot(trainDF['label_name'])
+    plt.show()
 
-    def distPerClass(self):
-        sns.countplot(x='label_name', data=self.df)
-        # sns.distplot(trainDF['label_name'])
+def typeDistPerClass(type):
+    typeDF=df[df['train/validation/test']==type]
+    if(len(typeDF)>0):
+        sns.countplot(x='label_name', data=typeDF)
         plt.show()
+    else:
+        print(type +' is empty')
 
-    def typeDistPerClass(self, type):
-        typeDF=self.df[self.df['train/validation/test']==type]
-        if(len(typeDF)>0):
-            sns.countplot(x='label_name', data=typeDF)
-            plt.show()
-        else:
-            print(type +' is empty')
+def countplot_distribution_of_train_validation_test():
+    sns.countplot(x=df['train/validation/test'],data=df)
+    plt.show()
 
-    def countplot_distribution_of_train_validation_test(self):
-        sns.countplot(x=self.df['train/validation/test'],data=self.df)
-        plt.show()
+def pie_distribution_of_train_validation_test(types,data):
+    # Creating explode data
+    explode = (0.1, 0.0, 0.2)
 
-    def pie_distribution_of_train_validation_test(self,types,data):
-        # Creating explode data
-        explode = (0.1, 0.0, 0.2)
+    # Creating color parameters
+    colors = ("orange", "cyan", "brown")
 
-        # Creating color parameters
-        colors = ("orange", "cyan", "brown")
+    # Wedge properties
+    wp = {'linewidth': 1, 'edgecolor': "green"}
 
-        # Wedge properties
-        wp = {'linewidth': 1, 'edgecolor': "green"}
-
-        # Creating autocpt arguments
-        def func(pct, allvalues):
-            absolute = int(pct / 100. * np.sum(allvalues))
-            return "{:.1f}%\n({:d} g)".format(pct, absolute)
+    # Creating autocpt arguments
+    def func(pct, allvalues):
+        absolute = int(pct / 100. * np.sum(allvalues))
+        return "{:.1f}%\n({:d} g)".format(pct, absolute)
 
         # Creating plot
-        fig, ax = plt.subplots(figsize=(10, 7))
-        wedges, texts, autotexts = ax.pie(data,
-                                          autopct=lambda pct: func(pct, data),
-                                          explode=explode,
-                                          labels=types,
-                                          shadow=True,
-                                          colors=colors,
-                                          startangle=90,
-                                          wedgeprops=wp,
-                                          textprops=dict(color="magenta"))
+    fig, ax = plt.subplots(figsize=(10, 7))
+    wedges, texts, autotexts = ax.pie(data,
+                                        autopct=lambda pct: func(pct, data),
+                                        explode=explode,
+                                        labels=types,
+                                        shadow=True,
+                                        colors=colors,
+                                        startangle=90,
+                                        wedgeprops=wp,
+                                        textprops=dict(color="magenta"))
+    # Adding legend
+    ax.legend(wedges, types,
+                title="types",
+                loc="center left",
+                bbox_to_anchor=(1, 0, 0.5, 1))
 
-        # Adding legend
-        ax.legend(wedges, types,
-                  title="types",
-                  loc="center left",
-                  bbox_to_anchor=(1, 0, 0.5, 1))
+    plt.setp(autotexts, size=8, weight="bold")
+    ax.set_title("train validation test distribution")
 
-        plt.setp(autotexts, size=8, weight="bold")
-        ax.set_title("train validation test distribution")
-
-        # show plot
-        plt.show()
-    def distribution_of_train_validation_test(self,distribution=True,pie=False):
-        # Creating dataset
-        types = ['train','validation','test']
-        data=[len(self.df[self.df['train/validation/test']=='train']),
-        len(self.df[self.df['train/validation/test']=='validation']),
-        len(self.df[self.df['train/validation/test']=='test'])]
-        if distribution==True:
-            self.countplot_distribution_of_train_validation_test()
-        if pie==True:
-            self.pie_distribution_of_train_validation_test(types, data)
+    # show plot
+    plt.show()
+def distribution_of_train_validation_test(distribution=True,pie=False):
+    # Creating dataset
+    types = ['train','validation','test']
+    data=[len(df[df['train/validation/test']=='train']),
+    len(df[df['train/validation/test']=='validation']),
+    len(df[df['train/validation/test']=='test'])]
+    if distribution==True:
+        countplot_distribution_of_train_validation_test()
+    if pie==True:
+        pie_distribution_of_train_validation_test(types, data)
 
 
 
 
 if __name__ == '__main__':
-    st=Statistics(r"C:\D\bootcamp\project\projectCode\df.csv")
-    # st.dataSize()
-    st.distPerClass()
-    st.typeDistPerClass('train')
-    st.typeDistPerClass('validation')
-    st.typeDistPerClass('test')
+    distPerClass()
+    typeDistPerClass('train')
+    typeDistPerClass('validation')
+    typeDistPerClass('test')
     # print(len(st.df[st.df['train/validation/test']=='train']))
-    st.distribution_of_train_validation_test(pie=True)
+    distribution_of_train_validation_test(pie=True)
