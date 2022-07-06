4a647b2009bc461b872cf119fd4a5c5bf2125ef3
Nechama <neproject2@gmail.com>
2022-07-06 18:30:05 +0300

    fix devide.py code

--- params.py
+++ params.py
@@ -1,6 +1,6 @@
 
 
 csv_cols = ['image_name', 'batch_label', 'original_label_number', 'current_label_number', 'label_name', 'dataset', 'train/validation/test']
-base_dir = r"C:\D\bootcamp\project\dataset"
-csv_path=r"C:\D\bootcamp\project\dataset\data.csv"
+base_dir = r"C:\פרוייקט בוטקמפ\dataset"
+csv_path = r"C:\פרוייקט בוטקמפ\projectCode\data.csv"
 chosen_label = [1, 2, 3, 4, 5]
