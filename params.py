2eac5ba402291c13ca8e83738e2a1ae576fbbccd
2022Group3 <group3databootcamp@gmail.com>
2022-07-06 18:22:15 +0300

    csv_path

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 
 
 csv_cols = ['image_name', 'batch_label', 'original_label_number', 'current_label_number', 'label_name', 'dataset', 'train/validation/test']
-base_dir = r"C:\פרוייקט בוטקמפ\dataset"
+base_dir = r"C:\D\bootcamp\project\dataset"
 chosen_label = [1, 2, 3, 4, 5]
