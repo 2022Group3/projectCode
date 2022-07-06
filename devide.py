b0baf005df471e8362c58198895aea6df3d80430
Nechama <neproject2@gmail.com>
2022-07-06 18:29:27 +0300

    fix devide.py code

--- devide.py
+++ devide.py
@@ -1,10 +1,12 @@
 #devide to train ,validation and test
 import pandas as pd
 import numpy as np
+import params
+
 
 def train_validate_test_split(csv_data, train_percent=0.6, validate_percent=0.1, seed=None):
     df = pd.read_csv(csv_data)
-    df.drop('train/validation/test', inplace=True, axis=1)
+    # df.drop('train/validation/test', inplace=True, axis=1)
     np.random.seed(seed)
     perm = np.random.permutation(df.index)
     m = len(df.index)
@@ -13,15 +15,14 @@ def train_validate_test_split(csv_data, train_percent=0.6, validate_percent=0.1,
     train = df.iloc[perm[:train_end]]
     validate = df.iloc[perm[train_end:validate_end]]
     test = df.iloc[perm[validate_end:]]
-    train['train/validation/test']='train'
-    validate['train/validation/test']='validation'
-    test['train/validation/test']='test'
-    dataframe=train.append(validate).append(test)
-    dataframe.to_csv('df.csv')
+    train[params.csv_cols[6]] = 'train'
+    validate[params.csv_cols[6]] = 'validation'
+    test[params.csv_cols[6]] = 'test'
+    dataframe = train.append(validate).append(test)
+    dataframe.to_csv(params.csv_path)
     return dataframe
 
 
-
 if __name__ == '__main__':
-    file=r"C:\D\bootcamp\project\projectCode\data.csv"
+    file = params.csv_path
     print(train_validate_test_split(file))
