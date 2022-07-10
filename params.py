179916b4c85bce100d7e11aea427a694bd194718
lea-cohen <l0548452382@gmail.com>
2022-07-10 13:05:20 +0300

    leales params

--- params.py
+++ params.py
@@ -1,8 +1,8 @@
 
 csv_cols = ['image_name', 'batch_label', 'original_label_number', 'current_label_number', 'label_name', 'dataset', 'train/validation/test']
-base_dir = r"D:\bootcamp\AMAT\project\dataset"
+base_dir = r"C:\bootcamp\project\dataset"
 chosen_label = [1, 2, 3, 4, 5]
 our_img_folderName="our_images_orginal"
 extract_img_folderName="datasetImages"
-CSV_path=r"D:\bootcamp\AMAT\project\data.csv"
+CSV_path=r"C:\bootcamp\project\data.csv"
 
